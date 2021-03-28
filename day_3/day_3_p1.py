7b5568c2b97b8bc9d395dd86d8ec28188bcd384f
Ken Kitts <kkitts2001@yahoo.com>
2021-03-28 16:20:16 -0700

    AoC Day 3, part 1 optimized for speed

--- day_3/day_3_p1.py
+++ day_3/day_3_p1.py
@@ -11,26 +11,27 @@ def draw_line(line, pos):
     """This function takes two list inputs, 'line' and 'pos'.
     The line input is the list of instructions to execute.
     The pos input is the positional coordinates from where to mapping
-    the line to a grid.  The return value is the grid (map)."""
-    grid = []
+    the line to a grid.  The return value is a set of coordinates
+    in a set data structure."""
+    grid = set()
     for i in line:
         direction = i[0]
         i = int(i.lstrip('UDLR'))
         if direction == 'U':
             for step in range(pos[1], pos[1] + i + 1):
-                grid.append((pos[0], step))
+                grid.add((pos[0], step))
             pos[1] = pos[1] + i
         elif direction == 'D':
             for step in range(pos[1], pos[1] - i - 1, -1):
-                grid.append((pos[0], step))
+                grid.add((pos[0], step))
             pos[1] = pos[1] - i
         elif direction == 'L':
             for step in range(pos[0], pos[0] - i - 1, -1):
-                grid.append((step, pos[1]))
+                grid.add((step, pos[1]))
             pos[0] = pos[0] - i
         elif direction == 'R':
             for step in range(pos[0], pos[0] + i + 1):
-                grid.append((step, pos[1]))
+                grid.add((step, pos[1]))
             pos[0] = pos[0] + i
     grid.remove((0, 0))
     return grid
@@ -38,11 +39,8 @@ def draw_line(line, pos):
 
 grid_a = draw_line(line_a, [0, 0])
 grid_b = draw_line(line_b, [0, 0])
-intersect = []
-for i in grid_a:
-    if i in grid_b:
-        intersect.append(i)
-closest_intersection = min([i[0] + abs(i[1]) for i in intersect])
+intersect = grid_a.intersection(grid_b)
+closest_intersection = min([abs(i[0]) + abs(i[1]) for i in intersect])
 stop_time = time.time()
 total_time = stop_time - start_time
 
